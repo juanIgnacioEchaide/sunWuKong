3ffbbed67f0a80bef81ae294d694e7bfda5484d2
juanIgnacioEchaide <jie_chaide@hotmail.com>
2021-05-24 22:29:33 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,173 @@
+import { GraphQLServer } from 'graphql-yoga'
+const mongoose = require('mongoose')
+import { Product, Ticket, Menu, Promo, User } from './model/models'
+ 
+/* TYPES QUERIES AND MUTATIONS */
+const typeDefs = `
+    type Query {
+        product(id: ID): [Product!]!
+        menu(id: ID): [Menu]!
+        ticket(author: String): [Ticket]!
+        user(email: String!, password: String!): User!
+        promo(id: ID): [Promo]!
+    }
+
+    type Mutation {
+        createProduct(description: String!, price: Float, expiringDate: String!): Product
+        createTicket(date: String, author: String, type: String, data: [ProductInput]): Ticket
+        createMenu(name: String, description: String, products: [ProductInput], price: Float): Menu
+        createPromo(name: String, description: String, menus: [MenuInput], price: Float): Promo
+        createUser(email: String, password: String, name: String, authorId: String): User
+    }
+  
+    type Product {
+        id: ID
+        description: String
+        price: Float
+        expiringDate: String
+    }
+
+    input ProductInput {
+        id: ID!
+        description: String!
+        price: Float
+        expiringDate: String!
+    }
+
+    type Menu {
+        id: ID
+        name: String
+        description: String
+        products: [Product]
+        price: Float
+    }
+
+    input MenuInput {
+        name: String
+        description: String
+        products: [ProductInput]
+        price: Float
+    }
+
+    type Promo {
+        id: ID
+        name: String
+        description: String
+        menus: [Menu]
+        price: Float
+    }
+
+    type Ticket {
+        id: ID
+        date: String
+        author: String
+        type: String
+        data: [Product]
+    }
+
+    type User {
+        id: ID,
+        email: String, 
+        password: String, 
+        name: String, 
+        authorId: String,
+    }
+`
+
+/* RESOLVERS FUNCTIONS */
+const resolvers = {
+    Query: {
+        product(parent, args,ctx, info) {
+            if(!args.id){
+            return Product.find({});
+            }
+            return Product.findOne({id: args.id});
+        },
+        menu(parent, args,ctx, info) {
+            if(!args.id){
+                return Menu.find({});
+                }
+                return Menu.findById(args.id)
+        },
+        ticket(parent, args,ctx, info) {
+            if(!args.author){
+                return Ticket.find({});
+            }
+             return Ticket.findOne({author: args.author});
+       },
+        promo(parent, args,ctx, info) {
+            if(!args.author){
+                return Promo.find({});
+            }
+             return Promo.findOne({id: args.id});
+       },
+        user(parent, args,ctx, info) {
+            return User.findOne({email:args.email, password: args.password});
+       }
+    },
+    Mutation: {
+        createProduct(parent,args, ctx, info){
+            let product = new Product({
+                id: args.id,
+                description: args.description,
+                price: args.price,
+                expiringDate: args.expiringDate
+            });
+            product.save();
+        },
+        createTicket(parent, args,ctx, info){
+            let ticket = new Ticket({
+                date: args.date,
+                author: args.author,
+                type: args.type,
+                data: args.data,
+            })
+            return ticket.save()
+        },
+        createMenu(parent, args,ctx, info){
+            let menu = new Menu({
+                name: args.name,
+                description: args.description,
+                products: args.products,
+                price: args.price,
+            })
+            return menu.save()
+        },
+        createPromo(parent, args,ctx, info){
+            let promo = new Promo({
+                name: args.name,
+                description: args.description,
+                menus: args.menus,
+                price: args.price,
+            })
+            return promo.save()
+        },
+        createUser(parent, args,ctx, info){
+            let user = new User({
+                email: args.email, 
+                password: args.password, 
+                name: args.name, 
+                authorId: args.authorId,
+            })
+            return user.save()
+        },
+    }
+}
+
+/* MONGOOSE CONNECTION */
+try {
+   mongoose.connect('mongodb+srv://devuser:1111@cluster0.duiue.mongodb.net/haozi', { useNewUrlParser: true, useUnifiedTopology: true });
+  } catch (error) {
+    err => console.log(err);
+ }
+mongoose.connection.once('open', function() {
+console.log('mongoose ok')
+});
+
+/* STARTING LOCAL HOST SERVER */
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers,
+})
+
+server.start( () => {console.log('graphql-yoga running on http://localhost:4000/')})
---
+++ src/model/models.js
@@ -0,0 +1,52 @@
+
+const mongoose = require('mongoose');
+const { Number, Schema, model, ObjectId } = mongoose;
+
+const productSchema = new Schema({
+    id: Number,
+    description: String, 
+    expiringDate: String,
+    price: Number, 
+})
+
+export const Product = model('Product', productSchema);
+
+const menuSchema = new Schema({
+    id: Number,
+    name: String, 
+    description: String, 
+    products: [productSchema],
+    price: Number, 
+})
+
+export const Menu = model('Menu', menuSchema);
+
+const promoSchema = new Schema({
+    id: Number,
+    name: String, 
+    description: String, 
+    menus: [menuSchema],
+    price: Number, 
+})
+
+export const Promo = model('Promo', promoSchema);
+
+const ticketSchema = new Schema({
+    id: Number,
+    author: String, 
+    type: String, 
+    date: String, 
+    data: [productSchema],
+})
+
+export const Ticket = model('Ticket', ticketSchema);
+
+const userSchema = new Schema({
+    id: Number,
+    email: String, 
+    password: String, 
+    name: String, 
+    authorId: String,
+})
+
+export const Ticket = model('User',userSchema);
