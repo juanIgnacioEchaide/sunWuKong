5174e36f6b71fe79388d63f8a0ef07871a90832c
juanIgnacioEchaide <jie_chaide@hotmail.com>
2021-07-06 20:22:55 -0300

    context object with user attached

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { GraphQLServer } from 'graphql-yoga'
 import { resolvers } from './resolvers'
 import { typeDefs  } from './typeDefs'
+import { getUserByToken } from './middleware/is-auth'
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 const isAuth = require('./middleware/is-auth.js')
@@ -20,13 +21,16 @@ const server = new GraphQLServer({
     typeDefs,
     resolvers,
     context: req => {
-        const authToken = req.request.headers.authorization
-        console.log('REQ', authToken);
+        const authToken = req.request.headers.authorization.split(' ')[1]
+        const user = getUserByToken(authToken)
+        if (!user) 
+            throw new AuthorizationError('you must be logged in')
+        return { user }   
     }
-
 });
 
 server.start( () => {
     console.log('graphql-yoga running on http://localhost:4000/')
 })
+
 
--- src/middleware/is-auth.js
+++ src/middleware/is-auth.js
@@ -1,29 +1,10 @@
 const jwt = require('jsonwebtoken')
+import { User } from '../model/models'
 
-module.exports = (req, res, next) => {
-    const authHeader = req.get('Authorization');
-        if( !authHeader ) {
-            req.isAuth = false
-            return next()
-        }  
-    const token = authHeader.split(' ')[1]
-        if( !token || token === ''){
-            req.isAuth = false
-            return next()
-        }
-    let decodedToken
-        try {
-            decodedToken = jwt.verify(token, 'somesupersecretkey')
-        }catch(err){
-            req.isAuth = false
-            return next()
-        }
-        if( !decodedToken){
-            req.isAuth = false
-            return next()
-        }
-    req.isAuth = true
-    req.userId = decodedToken.userId
-    next()
-    }
+export const getUserByToken = token => {
+    const decryptedToken = jwt.verify(token, 'supersecretkey')
+    const user = User.findOne({email:decryptedToken.email});
     
+        return user
+}
+   
